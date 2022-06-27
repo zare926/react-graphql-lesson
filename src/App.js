0463a54efd298cdd5c8e93eeec49684930d3f106
zare926 <ilove.boc926@gmail.com>
2022-06-27 20:35:48 +0900

    show search results

--- src/App.js
+++ src/App.js
@@ -44,7 +44,24 @@ function App() {
 
             const repositoryUnit = repositoryCount === 1 ? 'Repository' : 'Repositories'
             const title = `GitHub Repositories Search Results - ${repositoryCount} ${repositoryUnit}`
-            return <h2>{title}</h2>
+            return (
+              <React.Fragment>
+                <h2>{title}</h2>
+                <ul>
+                    {
+                      search.edges.map(edge => {
+                        const node = edge.node
+                        return (
+                          <li key={node.id}>
+                            <a href={node.url} target="_blank">{node.name}</a>
+                          </li>
+
+                        )
+                      })
+                    }
+                </ul>
+              </React.Fragment>
+            )
           }
         }
       </Query>
